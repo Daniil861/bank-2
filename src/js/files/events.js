fee7b4b350a235e73b56620fb40f1960078a36b8
igor Danchanka <one2pixel@yandex.ru>
2023-05-16 09:50:23 +0300

    separate logic show modal

--- src/js/files/events.js
+++ src/js/files/events.js
@@ -1,9 +1,5 @@
 import { initStartData } from './script.js';
 
-const modalItems = document.querySelectorAll('[data-modal]');
-const modal = document.querySelector('.modal');
-const modalId = modal.querySelector('.modal__id span');
-
 // Объявляем слушатель событий "клик"
 
 document.addEventListener('click', (e) => {
@@ -17,28 +13,10 @@ document.addEventListener('click', (e) => {
 		welcomeScreen.classList.add('_hide');
 	}
 
-	if (targetElement.closest('.modal__button') && modal.classList.contains('_visible')) {
-		modal.classList.remove('_visible');
-	}
-
-
 })
 
 
-if (modalItems.length) {
-	modalItems.forEach(button => {
-		button.addEventListener('click', () => {
 
-			if (button.dataset.id) {
-				const id = button.dataset.id;
-
-				modalId.textContent = id;
-
-				modal.classList.add('_visible');
-			}
-		})
-	})
-}
 
 
 
